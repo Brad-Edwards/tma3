12c747a1af6e953df196dec4fd3a2f49c6a5fb8a
Brad-Edwards <j.bradley.edwards@gmail.com>
2021-12-05 18:28:46 -0800

    Mod user, migrations not created

--- comp482_tma3/comp482_tma3/users/models.py
+++ comp482_tma3/comp482_tma3/users/models.py
@@ -5,12 +5,23 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+
+    class UserRoles(models.TextChoices):
+        CHILD = 'CHILD', _("Child")
+        PARENT = 'PARENT', _("Parent")
+        STAFF = 'STAFF', _("Staff")
+
     """Default user for COMP482 TMA3."""
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    role = CharField(max_length=6,
+                     choices=UserRoles.choices,
+                     default=UserRoles.child,
+                     )
+
 
     def get_absolute_url(self):
         """Get url for user's detail view.
--- comp482_tma3/config/settings/base.py
+++ comp482_tma3/config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "comp482_tma3.users",
     # Your stuff: custom apps go here
+    "roots",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
