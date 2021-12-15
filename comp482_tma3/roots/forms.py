78cb992f6a3bd755f42cefdfa59cae265883ff32
Brad-Edwards <j.bradley.edwards@gmail.com>
2021-12-15 12:15:09 -0800

    Removed unused classes, functions and imports

--- comp482_tma3/roots/forms.py
+++ comp482_tma3/roots/forms.py
@@ -1,12 +1,8 @@
 import datetime
-from dateutil.relativedelta import relativedelta
-import logging
+
 from django.core.exceptions import ValidationError
 
-import django.db.models
-from django.contrib import admin
-from django import forms as admin_forms
-from django.contrib.admin.widgets import FilteredSelectMultiple
+from django import forms as forms
 from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
 
@@ -14,14 +10,10 @@ from phonenumber_field.formfields import PhoneNumberField
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Button, ButtonHolder, Div, Fieldset, HTML, Layout, Submit
 
-from roots.models import Child, Classroom, \
-    ContactInfo, Family, Food, Meal, Menu, Nap, Parent, Person, \
-    Registration, Toileting
-
-class DateInput(admin_forms.widgets.DateInput):
+class DateInput(forms.widgets.DateInput):
     input_type = 'date'
 
-class CheckInForm(admin_forms.Form):
+class CheckInForm(forms.Form):
     class Children(TextChoices):
         T = 'Tina', _("Tina")
         L = 'Lotanna', _("Lotanna")
@@ -39,11 +31,11 @@ class CheckInForm(admin_forms.Form):
         if input_time < opening:
             raise ValidationError("Cannot check in before the centre opens at %s." % opening.strftime('%I:%M %p'))
 
-    children = admin_forms.MultipleChoiceField(label="Children to Check In", required=True, choices = Children.choices,
-                                               widget=admin_forms.CheckboxSelectMultiple())
-    check_in_date = admin_forms.DateField(widget=admin_forms.DateInput(attrs={'type': 'date'}), required=True,
+    children = forms.MultipleChoiceField(label="Children to Check In", required=True, choices = Children.choices,
+                                               widget=forms.CheckboxSelectMultiple())
+    check_in_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True,
                                           initial=datetime.datetime.now().strftime('%Y-%m-%d'))
-    check_in_time = admin_forms.TimeField(widget=admin_forms.DateInput(attrs={'type': 'time'}), required=True,
+    check_in_time = forms.TimeField(widget=forms.DateInput(attrs={'type': 'time'}), required=True,
                                           initial=datetime.datetime.now().strftime('%I:%M'),
                                           validators=[validate_check_in_time])
 
@@ -79,17 +71,17 @@ class CheckInForm(admin_forms.Form):
         )
 
 
-class CheckOutForm(admin_forms.Form):
+class CheckOutForm(forms.Form):
     class Children(TextChoices):
         T = 'Tina', _("Tina")
         L = 'Lotanna', _("Lotanna")
         M = 'Mei', _("Mei")
 
-    children = admin_forms.MultipleChoiceField(label="Children to Check Out", required=True, choices = Children.choices,
-                                               widget=admin_forms.CheckboxSelectMultiple())
-    check_out_date = admin_forms.DateField(widget=admin_forms.DateInput(attrs={'type': 'date'}), required=True,
+    children = forms.MultipleChoiceField(label="Children to Check Out", required=True, choices = Children.choices,
+                                               widget=forms.CheckboxSelectMultiple())
+    check_out_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True,
                                           initial=datetime.datetime.now().strftime('%Y-%m-%d'))
-    check_out_time = admin_forms.TimeField(widget=admin_forms.DateInput(attrs={'type': 'time'}), required=True,
+    check_out_time = forms.TimeField(widget=forms.DateInput(attrs={'type': 'time'}), required=True,
                                           initial=datetime.datetime.now().strftime('%I:%M'))
 
     def __init__(self, *args, **kwargs):
@@ -123,86 +115,19 @@ class CheckOutForm(admin_forms.Form):
         )
 
 
-class ChildAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Child
-        fields = '__all__'
-
-
-class ClassroomAdminForm(admin_forms.ModelForm):
-    kids = admin_forms.ModelMultipleChoiceField(queryset=Child.objects.all(),
-                                                label=_("Children"),
-                                                required=False,
-                                                widget=FilteredSelectMultiple(
-                                                    _("kids"),
-                                                    False,
-                                                ))
-
-    class Meta:
-        model = Classroom
-        fields = ['name', 'kids', 'short_name']
-
-    def __init(self, *args, **kwargs):
-        super(ClassroomAdminForm, self).__init__(*args, **kwargs)
-        self.fields['kids'].queryset = Child.objects.all()
-
-
-class ContactInfoAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = ContactInfo
-        fields = '__all__'
-
-
-class FamilyAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Family
-        fields = '__all__'
-
-
-class FoodAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Food
-        fields = '__all__'
-
-
-class MealAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Meal
-        fields = '__all__'
-
-
-class MenuAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Menu
-        fields = '__all__'
-        food = admin_forms.ModelMultipleChoiceField(queryset=Food.objects.all(),
-                                                label=_("Menu Items"),
-                                                required=False,
-                                                widget=FilteredSelectMultiple(
-                                                    _("food"),
-                                                    False,
-                                                ))
-
-
-class NapAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Nap
-        fields = '__all__'
-
-
-class NapForm(admin_forms.Form):
+class NapForm(forms.Form):
     class Children(TextChoices):
         T = 'Tina', _("Tina")
         L = 'Lotanna', _("Lotanna")
         M = 'Mei', _("Mei")
 
-    children = admin_forms.MultipleChoiceField(label="Children", required=True, choices = Children.choices,
-                                               widget=admin_forms.CheckboxSelectMultiple())
-    nap_date = admin_forms.DateField(widget=admin_forms.DateInput(attrs={'type': 'date'}), required=True,
+    children = forms.MultipleChoiceField(label="Children", required=True, choices = Children.choices,
+                                               widget=forms.CheckboxSelectMultiple())
+    nap_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True,
                                           initial=datetime.datetime.now().strftime('%Y-%m-%d'))
-    nap_start_time = admin_forms.TimeField(widget=admin_forms.DateInput(attrs={'type': 'time'}), required=True,
+    nap_start_time = forms.TimeField(widget=forms.DateInput(attrs={'type': 'time'}), required=True,
                                           initial=datetime.datetime.now().strftime('%I:%M'))
-    nap_end_time = admin_forms.TimeField(widget=admin_forms.DateInput(attrs={'type': 'time'}), required=True,
+    nap_end_time = forms.TimeField(widget=forms.DateInput(attrs={'type': 'time'}), required=True,
                                            initial=datetime.datetime.now().strftime('%I:%M'))
 
     def __init__(self, *args, **kwargs):
@@ -243,25 +168,7 @@ class NapForm(admin_forms.Form):
         )
 
 
-class ParentAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Parent
-        fields = '__all__'
-
-
-class PersonAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Person
-        fields = '__all__'
-
-
-class RegistrationAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Registration
-        fields = '__all__'
-
-
-class RegisterChildForm(admin_forms.Form):
+class RegisterChildForm(forms.Form):
     class Classrooms(TextChoices):
         JKL1 = 'JKL1', _('Junior Kindergarten Level 1')
         JKL2 = 'JKL2', _('Junior Kindergarten Level 2')
@@ -283,52 +190,52 @@ class RegisterChildForm(admin_forms.Form):
         YT = "YT", _("YT")
 
 
-    child_classroom = admin_forms.ChoiceField(
+    child_classroom = forms.ChoiceField(
         label = "Classroom",
-        widget = admin_forms.Select(),
+        widget = forms.Select(),
         choices = Classrooms.choices,
         required = True,
     )
 
-    child_first_name = admin_forms.CharField(label="First Name", max_length=255, required=True)
-    child_last_name = admin_forms.CharField(label="Last Name", max_length=266, required=True)
-    child_date_of_birth = admin_forms.DateField(label="Date of Birth", widget=admin_forms.DateInput(
+    child_first_name = forms.CharField(label="First Name", max_length=255, required=True)
+    child_last_name = forms.CharField(label="Last Name", max_length=266, required=True)
+    child_date_of_birth = forms.DateField(label="Date of Birth", widget=forms.DateInput(
         attrs={'type': 'date'}))
 
-    child_lives_with_guardian1 = admin_forms.BooleanField(label="Child's Primary Address", required=False)
-    child_lives_with_guardian2 = admin_forms.BooleanField(label="Child's Primary Address", required=False)
-    guardian1_first_name = admin_forms.CharField(label="First Name", max_length=255, required=True)
-    guardian1_last_name = admin_forms.CharField(label="Last Name", max_length=266, required=True)
-    guardian1_date_of_birth = admin_forms.DateField(label="Date of Birth", widget=admin_forms.DateInput(
+    child_lives_with_guardian1 = forms.BooleanField(label="Child's Primary Address", required=False)
+    child_lives_with_guardian2 = forms.BooleanField(label="Child's Primary Address", required=False)
+    guardian1_first_name = forms.CharField(label="First Name", max_length=255, required=True)
+    guardian1_last_name = forms.CharField(label="Last Name", max_length=266, required=True)
+    guardian1_date_of_birth = forms.DateField(label="Date of Birth", widget=forms.DateInput(
         attrs={'type': 'date',}))
-    guardian1_address = admin_forms.CharField(label="Address", max_length=255, required=True)
-    guardian1_city = admin_forms.CharField(label="City", max_length=255, required=True, initial="Chilliwack")
-    guardian1_province = admin_forms.ChoiceField(
-        widget = admin_forms.Select(),
+    guardian1_address = forms.CharField(label="Address", max_length=255, required=True)
+    guardian1_city = forms.CharField(label="City", max_length=255, required=True, initial="Chilliwack")
+    guardian1_province = forms.ChoiceField(
+        widget = forms.Select(),
         choices=Provinces.choices,
         initial = 'BC',
         required=True)
-    guardian1_country = admin_forms.CharField(label="Country", max_length=255, required=True, initial="Canada")
-    guardian1_postal_code = admin_forms.CharField(label="Postal Code", max_length=255, required=True)
-    guardian1_phone1 = PhoneNumberField(widget=admin_forms.TextInput(attrs={}), label="Primary Phone Number", required=True)
-    guardian1_phone2 = PhoneNumberField(widget=admin_forms.TextInput(attrs={}), label="Alternate Phone Number", required=False)
+    guardian1_country = forms.CharField(label="Country", max_length=255, required=True, initial="Canada")
+    guardian1_postal_code = forms.CharField(label="Postal Code", max_length=255, required=True)
+    guardian1_phone1 = PhoneNumberField(widget=forms.TextInput(attrs={}), label="Primary Phone Number", required=True)
+    guardian1_phone2 = PhoneNumberField(widget=forms.TextInput(attrs={}), label="Alternate Phone Number", required=False)
 
-    guardian2_first_name = admin_forms.CharField(label="First Name", max_length=255, required=False)
-    guardian2_last_name = admin_forms.CharField(label="Last Name", max_length=266, required=False)
-    guardian2_date_of_birth = admin_forms.DateField(label="Date of Birth", widget=admin_forms.DateInput(
+    guardian2_first_name = forms.CharField(label="First Name", max_length=255, required=False)
+    guardian2_last_name = forms.CharField(label="Last Name", max_length=266, required=False)
+    guardian2_date_of_birth = forms.DateField(label="Date of Birth", widget=forms.DateInput(
         attrs={'type': 'date', }), required=False)
-    guardian2_address = admin_forms.CharField(label="Address", max_length=255, required=False)
-    guardian2_city = admin_forms.CharField(label="City", max_length=255, required=False, initial="Chilliwack")
-    guardian2_province = admin_forms.ChoiceField(
-        widget=admin_forms.Select(),
+    guardian2_address = forms.CharField(label="Address", max_length=255, required=False)
+    guardian2_city = forms.CharField(label="City", max_length=255, required=False, initial="Chilliwack")
+    guardian2_province = forms.ChoiceField(
+        widget=forms.Select(),
         choices=Provinces.choices,
         initial='BC',
         required=False)
-    guardian2_country = admin_forms.CharField(label="Country", max_length=255, required=False, initial="Canada")
-    guardian2_postal_code = admin_forms.CharField(label="Postal Code", max_length=255, required=False)
-    guardian2_phone1 = PhoneNumberField(widget=admin_forms.TextInput(attrs={}), label="Primary Phone Number",
+    guardian2_country = forms.CharField(label="Country", max_length=255, required=False, initial="Canada")
+    guardian2_postal_code = forms.CharField(label="Postal Code", max_length=255, required=False)
+    guardian2_phone1 = PhoneNumberField(widget=forms.TextInput(attrs={}), label="Primary Phone Number",
                                         required=False)
-    guardian2_phone2 = PhoneNumberField(widget=admin_forms.TextInput(attrs={}), label="Alternate Phone Number",
+    guardian2_phone2 = PhoneNumberField(widget=forms.TextInput(attrs={}), label="Alternate Phone Number",
                                         required=False)
 
     def __init__(self, *args, **kwargs):
@@ -504,7 +411,7 @@ class RegisterChildForm(admin_forms.Form):
         )
 
 
-class ToiletingForm(admin_forms.Form):
+class ToiletingForm(forms.Form):
     class Children(TextChoices):
         T = 'Tina', _("Tina")
         L = 'Lotanna', _("Lotanna")
@@ -518,15 +425,14 @@ class ToiletingForm(admin_forms.Form):
         NONE = 'None', _("None")
 
 
-    children = admin_forms.MultipleChoiceField(label="Children", required=True, choices = Children.choices,
-                                               widget=admin_forms.CheckboxSelectMultiple())
-    toileting_date = admin_forms.DateField(widget=admin_forms.DateInput(attrs={'type': 'date'}), required=True,
+    children = forms.MultipleChoiceField(label="Children", required=True, choices = Children.choices,
+                                               widget=forms.CheckboxSelectMultiple())
+    toileting_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True,
                                           initial=datetime.datetime.now().strftime('%Y-%m-%d'))
-    toileting_time = admin_forms.TimeField(widget=admin_forms.DateInput(attrs={'type': 'time'}), required=True,
+    toileting_time = forms.TimeField(widget=forms.DateInput(attrs={'type': 'time'}), required=True,
                                           initial=datetime.datetime.now().strftime('%I:%M'))
-    results = admin_forms.MultipleChoiceField(label="Results", required=True, choices=Options.choices,
-                                               widget=admin_forms.CheckboxSelectMultiple())
-
+    results = forms.MultipleChoiceField(label="Results", required=True, choices=Options.choices,
+                                               widget=forms.CheckboxSelectMultiple())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -564,9 +470,3 @@ class ToiletingForm(admin_forms.Form):
                 css_class = 'container w-50 mt-5'
             ),
         )
-
-
-class ToiletingAdminForm(admin_forms.ModelForm):
-    class Meta:
-        model = Toileting
-        fields = '__all__'
--- comp482_tma3/roots/views.py
+++ comp482_tma3/roots/views.py
@@ -1,30 +1,13 @@
-import datetime
-import logging
-
 from django import forms
-from django.contrib import admin
-from django.db import models
-from django.contrib.admin import widgets as adminWidget
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import Context, loader
-from django.views.generic.edit import CreateView, FormView
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout
-
-from roots.models import Child, Classroom, ContactInfo, Family, Parent, Person, Registration
-from roots.forms import CheckInForm, CheckOutForm, NapForm, RegisterChildForm, ToiletingForm
+from forms import CheckInForm, CheckOutForm, NapForm, RegisterChildForm, ToiletingForm
 
 from django.urls import reverse
-from django.utils.safestring import mark_safe
-from django.forms import widgets
-from django.conf import settings
 
 # Create your views here.
-def attendance(request, attendance_id):
-    return HttpResponse("You found attendance %s" % attendance_id)
-
 def check_in(request):
     form = CheckInForm(request.POST or None)
     if request.method == 'POST':
@@ -77,31 +60,10 @@ def check_out_success(request):
 
     return render(request, "roots/check_out_success.html", {'data': data, 'kids': c})
 
-def children(request, child_id):
-    return HttpResponse(f'You found child {child_id}')
-
-def classroom(request, classroom_name):
-    return HttpResponse("You found classroom %s" % classroom_name)
-
-def contact_info(request, contact_info_id):
-    return HttpResponse("You found contact info %s" % contact_info_id)
-
-def families(request, family_id):
-    return HttpResponse("You found family info %s" % family_id)
-
-def food(request, food_id):
-    return HttpResponse("You found food %s" % food_id)
-
 def index(request):
     template = loader.get_template("roots/index.html")
     context = Context({'moo': "moo"})
     return HttpResponse(template.render(context, request))
-
-def menu(request, menu_id):
-    return HttpResponse("You found menu %s" % menu_id)
-
-def meal(request, meal_id):
-    return HttpResponse("You found meal %s" % meal_id)
 
 def nap(request):
     form = NapForm(request.POST or None)
@@ -129,15 +91,9 @@ def nap_success(request):
 
     return render(request, "roots/nap_success.html", {'data': data, 'kids': c})
 
-def parents(request, parent_id):
-    return HttpResponse(f'You found parent {parent_id}')
-
-def people(request, person_id):
-    return HttpResponse(f'You found person {person_id}')
-
-
 class DateInput(forms.DateInput):
     input_type = 'date'
+
 
 def register(request):
     form = RegisterChildForm(request.POST or None)
@@ -151,9 +107,6 @@ def register(request):
 def register_success(request):
     data = request.session.get('register_child_data',  None)
     return render(request, "roots/register_success.html", {'data': data})
-
-def registration(request, registration_id):
-    return HttpResponse(f'You found {registration_id}')
 
 def registration_landing(request):
     template = loader.get_template("roots/registration_landing.html")
@@ -185,4 +138,3 @@ def toileting_success(request):
         c.append(kids[-1])
 
     return render(request, "roots/toileting_success.html", {'data': data, 'kids': c})
-
